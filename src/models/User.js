339fac99704a38ab04cac3f9442a82da6594e814
Naiara Martins <naiaraxf@gmail.com>
2023-08-25 21:31:49 -0300

    Challenge 2 done :heavy_check_mark:

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING
+    },
+    image: {
+      type: DataTypes.STRING
+    }
+  }, {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true
+  });
+
+  return User;
+};
